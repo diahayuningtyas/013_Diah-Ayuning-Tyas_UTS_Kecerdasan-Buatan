05dae1e87b246598a28ac6108d907686ec36ae4e
diahayuningtyas <90167777+diahayuningtyas@users.noreply.github.com>
2022-10-19 13:49:59 +0700

    Update 013_Diah Ayuning Tyas_Multi Neuron.py

--- 013_Diah Ayuning Tyas_Multi Neuron.py
+++ 013_Diah Ayuning Tyas_Multi Neuron.py
@@ -4,21 +4,21 @@
 #inisialisasi numpy
 import numpy as np
 
-#inisialisasi variabel dengan input sesuai panjang input layer = 10
+#Inisialisasi variabel dengan input sesuai panjang input layer = 10
 inputs = [2.0,1.5,2.2,3.4,1.0,3.0,4.5,5.0,2.4,7.0]
 
-#inisialisasi bobot variabel dengan weight sesuai jumlah neuron = 5 
+#Inisialisasi bobot variabel dengan weight sesuai jumlah neuron = 5 
 weights = [[5.5,0.8,-1.2,3.5,4,8,-1.5,-2.2,0.4,5.5],
            [3,6,0.7,-2.4,1.0,5,2.5,0.4,-0.6,9.0],
            [2,1.5,2.0,-0.5,0.2,-2.2,1.0,7,5.5,0.5],
            [1.2,4,8,1,3,0.2,8.0,7.5,-0.3,4.5],
            [3.3,1.5,-4.2,3.0,5,7,0.1,2.4,1.5,6.5]]
 
-#jumlah bias seperti jumlah neuron 
+#Jumlah bias seperti jumlah neuron 
 bias = [1.2,0.4,5,2.5,0.2] 
 
-#menghitung output = (input*weight)+bias
+#Menghitung output = (input*weight)+bias
 output = np.dot(weights, inputs) + bias
 
-#mencetak output 
+#Mencetak output 
 print(output)
